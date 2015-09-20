a736e7d24ee69036adca08c7289194f15cc4766e
Michal Piekarczyk <namoopsoo>
2015-09-20 16:19:00 -0400

    cleanup

--- myrun.py
+++ myrun.py
@@ -1,24 +1,15 @@
 from flask import Flask, request, redirect
 import twilio.twiml
 
-# from quickstart_google_cal import MyCalendar
 from responder import ServerResponder
-from utils import form_twiml_reply
 
-
-import settings
  
 app = Flask(__name__)
  
 @app.route("/reply", methods=['GET', 'POST'])
 def main_twilio_entrypoint():
-
     r = ServerResponder(request.values)
-
     reply = r.handle_message()
-
-    # response = form_twiml_reply(reply)
-    # return response
     return reply
 
 
